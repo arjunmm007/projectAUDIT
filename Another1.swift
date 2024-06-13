5e282ddcc71e7beebf108e3190c0743cb8592580
arjunmm007 <magnumshark100@gmail.com>
2024-06-13 15:49:21 -0500

    themes added. many changes made

---
+++ Another1.swift
@@ -0,0 +1,18 @@
+//
+//  Another1.swift
+//  projectAUDIT
+//
+//  Created by Arjun Mehta on 6/13/24.
+//
+
+import SwiftUI
+
+struct Another1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Another1()
+}
---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  projectAUDIT
+//
+//  Created by Arjun Mehta on 6/13/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
